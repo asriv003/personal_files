137c8d93d5787d83897c763ce58fd798bf803d38
Abhishek Kumar Srivastava <asriv003@ucr.edu>
2017-09-27 17:18:19 -0700

    template file

---
+++ template.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <queue>
+#include <algorithm>
+#include <vector>
+#include <cstring>
+#include <stack>
+#include <cctype>
+#include <utility>   
+#include <map>
+#include <string>  
+#include <climits> 
+#include <set>
+#include <string>    
+#include <sstream>
+#include <utility>   
+#include <ctime>
+#include <cassert>
+#include <fstream>
+#include <iomanip>
+using namespace std;
+
+typedef long long LL;
+typedef vector<int> VI;
+typedef pair<int, int> II;
+typedef vector<II> VII;
+typedef vector<long long> VLL;
+typedef vector<bool> VB;
+
+#define SZ(A) ((int)A.size())
+#define LEN(A) ((int)A.length())
+#define MS(A) memset(A, 0, sizeof(A))
+#define MSV(A,a) memset(A, a, sizeof(A))
+#define MAX(a,b) ((a >= b) ? (a) : (b))
+#define MIN(a,b) ((a >= b) ? (b) : (a))
+#define ABS(a) (((a) > 0) ? (a) : (-a))
+#define MP make_pair
+#define X first
+#define Y second
+#define PB push_back
+#define FOUND(A, x) (A.find(x) != A.end())
+#define INF (int(1e9))
+#define INFL (LL(1e18))
+#define EPS 1e-12
+
+#define chkbit(s, b) (s & (1<<b))
+#define setbit(s, b) (s |= (1<<b))
+#define clrbit(s, b) (s &= ~(1<<b))
+
+#define REP(i, n) for(int i = 0; i < (n); i++)
+#define FOR(i, a, n) for(int i = a; i < n; i++)
+#define REV(i, a, n) for(int i = a; i > n; i--)
+#define FORALL(itr, c) for(itr = (c).begin(); itr != (c).end(); itr++)
+#define ALL(A) A.begin(), A.end()
+#define LLA(A) A.rbegin(), A.rend()
+//int dx[] = {-1, 0, 1, 0}, dy[] = {0, 1, 0, -1};
+//int dx[] = {1, 1, 1, 0, 0, -1, -1, -1}, dy[] = {1, 0, -1, 1, -1, 1, 0, -1};
+
+int main() {
+	std::ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	//CODE
+    return 0;
+}
